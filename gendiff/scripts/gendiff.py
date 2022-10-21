5744b11b43b4f5ece524a7f1837045b7cabc1d9c
stigsanek <alexander.e.egorov@pikta.ru>
2022-10-21 12:59:18 +0400

    Adds argument

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,6 +9,7 @@ def create_args_parser() -> argparse.ArgumentParser:
     :return: argparse.ArgumentParser
     """
     parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser.add_argument("-f", "--format", help="set format of output")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     return parser
