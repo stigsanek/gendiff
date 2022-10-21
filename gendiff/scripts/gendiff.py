d178f571c3ace061c3d3be195c2592d6625476a3
stigsanek <alexander.e.egorov@pikta.ru>
2022-10-21 12:36:15 +0400

    Creates args parser function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+import argparse
+
+
+def create_args_parser() -> argparse.ArgumentParser:
+    """
+    Create arguments parser object
+
+    :return: argparse.ArgumentParser
+    """
+    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    return parser
+
+
+def main():
+    """
+    Main function
+
+    :return:
+    """
+    parser = create_args_parser()
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
